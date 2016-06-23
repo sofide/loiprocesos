2ef5ee8ef3aebe84c886d591908506a21a642fdf
Sofia Denner <sofi.denner@gmail.com>
2016-06-22 23:59:01 -0300

    se agregaron modelos grupo y pertenencia en la app Grupos y se eliminaron lineas duplicades en clases.models

--- clases/models.py
+++ clases/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-from django.db import models
-from django.utils import timezone
-
 
 class Clase(models.Model):
     fecha = models.DateTimeField(default=timezone.now)
--- grupos/models.py
+++ grupos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Grupo(models.Model):
+    año = models.IntegerField()
+    numero = models.IntegerField()
+    empresa = models.CharField(max_length=200)
+    producto = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(str(self.año), self.empresa, self.producto)
+
+
+class Pertenencia(models.Model):
+    grupo = models.ForeignKey(Grupo)
+    usuario = models.ForeignKey('auth.User')
