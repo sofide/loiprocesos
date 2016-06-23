666dcd255aa2a691a971155ecfd02ddfd698573c
Sofia Denner <sofi.denner@gmail.com>
2016-06-22 23:10:21 -0300

    modelos Grupos e Integrantes eliminados del admin de clases

--- clases/admin.py
+++ clases/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from clases.models import (
     Clase,
-    Grupo,
-    Integrante,
     TP,
     Exposicion,
     Pregunta,
 )
 
 admin.site.register(Clase)
-admin.site.register(Grupo)
-admin.site.register(Integrante)
 admin.site.register(TP)
 admin.site.register(Exposicion)
 admin.site.register(Pregunta)
