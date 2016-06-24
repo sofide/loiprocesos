50353dd21a7affbc9d0d518fb26b432df8a817ce
Sofia Denner <sofi.denner@gmail.com>
2016-06-23 23:44:31 -0300

    add a / in clases url

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^clases', include('clases.urls')),
+    url(r'^clases/', include('clases.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
