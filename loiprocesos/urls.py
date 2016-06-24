ae1c09627894dbcf245d542872b3c116b34c6f6a
Sofia Denner <sofi.denner@gmail.com>
2016-06-23 23:39:32 -0300

    configuracion de las url /cuentas delegado a registration

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^clases', include('clases.urls')),
+    url(r'^cuentas/', include('registration.backends.hmac.urls')),
 ]
