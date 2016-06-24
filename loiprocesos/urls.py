a62ea48d277d6ad690496b95218cf8587e2a1832
Sofia Denner <sofi.denner@gmail.com>
2016-06-23 23:43:46 -0300

    cambio cuentas/ por /accounts en urls

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^clases', include('clases.urls')),
-    url(r'^cuentas/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
