bd0aaef20de7cdb1f353a3f2bceb3309bba61099
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 00:50:39 -0300

    changed the way to import home view from base app

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-import base
+from base.views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^clases/', include('clases.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^/$', base.views.home, name='home'),
+    url(r'^/$', home, name='home'),
 ]
