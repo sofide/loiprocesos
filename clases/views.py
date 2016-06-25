bba7d703fbc17836204413a511ce71afbd6193a8
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 02:07:30 -0300

    added clases home view

--- clases/views.py
+++ clases/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def clases_principal(request):
-    return render(request, 'clases/clases_principal.html', {})
+def clases_home(request):
+    return render(request, 'clases/clases_home.html', {})
