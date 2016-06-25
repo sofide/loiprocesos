e873ed8aaac31a2d04b2e0e7c2e5f1bf10f2c37e
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 20:21:31 -0300

    fixed clases viewer view

--- clases/views.py
+++ clases/views.py
@@ -21,17 +21,18 @@ def clases_home(request):
 
 
 def ver_clase(request, pk):
+    clase = get_object_or_404(Clase, pk=pk)
+    exposiciones = Exposicion.objects.filter(clase = clase).order_by('grupo__numero')
     if request.method == "POST":
             form = ExposicionForm(request.POST)
             if form.is_valid():
-                exposicion = form.save()
+                exposicion = form.save(commit=False)
+                exposicion.clase = clase
                 exposicion.save()
                 return redirect('clases.views.ver_clase', pk=pk)
     else:
-        clase = get_object_or_404(Clase, pk=pk)
-        exposiciones = Exposicion.objects.filter(clase__id = pk).order_by('grupo__numero')
         form = ExposicionForm()
-    
+
     return render(
         request,
         'clases/ver_clase.html',
