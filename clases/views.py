e24784949d625b38ff46cb38a3ad27474d3d04b7
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 04:01:02 -0300

    added ClaseForm to the view clases_home in clases app

--- clases/views.py
+++ clases/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from clases.forms import ContadorPreguntasForm
+from django.shortcuts import render, redirect
+from clases.forms import ContadorPreguntasForm, ClaseForm
+from clases.models import Clase
+
 
 def clases_home(request):
-    return render(request, 'clases/clases_home.html', {})
+    clases = Clase.objects.all()
+    if request.method == "POST":
+            form = ClaseForm(request.POST)
+            if form.is_valid():
+                clase = form.save()
+                clase.save()
+                return redirect('clases.views.clases_home')
+    else:
+        form = ClaseForm()
+    return render(
+        request,
+        'clases/clases_home.html',
+        {'clases': clases, 'form': form}
+    )
+
 
 def clases_add(request):
     form = ContadorPreguntasForm()
