1072ee8e7d5e3d5b7e374b2b49b0cd128ac5995f
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 02:58:56 -0300

    added ContadorPreguntas form to clases_add view

--- clases/views.py
+++ clases/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from clases.forms import ContadorPreguntasForm
 
 def clases_home(request):
     return render(request, 'clases/clases_home.html', {})
 
 def clases_add(request):
-    return render(request, 'clases/clases_add.html', {})
+    form = ContadorPreguntasForm()
+    return render(request, 'clases/clases_add.html', {'form': form})
