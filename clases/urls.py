451debec28085b1c51776ae299544dd279e416c5
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 20:17:38 -0300

    add exposicion url in urls.py, clases app

--- clases/urls.py
+++ clases/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
-from clases.views import clases_home, clases_add, ver_clase
+from clases.views import clases_home, clases_add, ver_clase, ver_exposicion
 
 urlpatterns = [
     url(r'^$', clases_home, name='clases_home'),
     url(r'^cargar$', clases_add, name='clases_add'),
-    url(r'^(?P<pk>[0-9]+)/$', ver_clase, name='ver_clase' )
+    url(r'^(?P<pk>[0-9]+)/$', ver_clase, name='ver_clase'),
+    url(r'^exposicion/(?P<expo_pk>[0-9]+)/$', ver_exposicion, name='ver_exposicion'),
 
 ]
