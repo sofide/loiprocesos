c26f6962aba32a470eaae11d57f33f9f663606e9
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 19:13:49 -0300

    added viewer clases url in clases app

--- clases/urls.py
+++ clases/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from clases.views import clases_home, clases_add
+from clases.views import clases_home, clases_add, ver_clase
 
 urlpatterns = [
     url(r'^$', clases_home, name='clases_home'),
     url(r'^cargar$', clases_add, name='clases_add'),
+    url(r'^(?P<pk>[0-9]+)/$', ver_clase, name='ver_clase' )
 
 ]
