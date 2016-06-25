3bad49e1d350558defb6633b79e835ea6e96db17
Sofia Denner <sofi.denner@gmail.com>
2016-06-25 01:02:44 -0300

    typo url home fixed

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^clases/', include('clases.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^/$', home, name='home'),
+    url(r'^$', home, name='home'),
 ]
