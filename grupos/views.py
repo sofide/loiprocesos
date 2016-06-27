596285f724c77a91b77acb2b56e6ffdadf64ce4f
Sofia Denner <sofi.denner@gmail.com>
2016-06-27 00:30:01 -0300

    added grupos_home and ver_grupos views

--- grupos/views.py
+++ grupos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+import datetime
+from grupos.models import Grupo
 
-# Create your views here.
+def grupos_home(request, año=datetime.datetime.now().strftime('%Y')):
+    grupos = Grupo.objects.filter(año = año).order_by('numero')
+
+    return render(request, 'grupos/grupos_home.html', {'grupos': grupos, 'año': año })
+
+
+def ver_grupo(request, grupo_pk):
+    grupo = get_object_or_404(Grupo, pk=grupo_pk)
+
+    return render(request, 'grupos/ver_grupo.html', {'grupo': grupo,})
