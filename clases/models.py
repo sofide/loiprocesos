40c4affa0be6076cbf1ea44fa4cec7151234899e
Sofia Denner <sofi.denner@gmail.com>
2016-06-27 23:09:43 -0300

    added datetimes fields in Exposicion model (for timing expo and pregs duration)

--- clases/models.py
+++ clases/models.py
@@ -20,6 +20,10 @@ class Exposicion(models.Model):
     clase = models.ForeignKey(Clase)
     grupo = models.ForeignKey('grupos.Grupo', null=True)
     tp = models.ForeignKey(TP)
+    start_expo = models.DateTimeField(null=True, default=null)
+    start_preg = models.DateTimeField(null=True, default=null)
+    finish_expo = models.DateTimeField(null=True, default=null)
+
 
     class Meta:
         verbose_name_plural = "Exposiciones"
