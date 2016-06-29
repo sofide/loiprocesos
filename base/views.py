dc1bbedd34b2b555dc3a5b7387c88c7dd63c1c1e
Sofia Denner <sofi.denner@gmail.com>
2016-06-29 00:55:38 -0300

    added pruebas view

--- base/views.py
+++ base/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html', {})
+
+
+def pruebas(request):
+    return render(request, 'base/pruebas.html')
