4c0d31da78f68bafd9d25095846c2812c4fdb5f8
Sofia Denner <sofi.denner@gmail.com>
2016-06-29 00:58:25 -0300

    added url for pruebas

--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from base.views import home
+from base.views import home, pruebas
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^$', home, name='home'),
+    url(r'^pruebas/$', pruebas, name='pruebas'),
     url(r'^clases/', include('clases.urls')),
     url(r'^grupos/', include('grupos.urls')),
 ]
