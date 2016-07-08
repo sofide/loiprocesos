59b038992a054af034a897ca31c3d5acd1c8b863
Sofia Denner <sofi.denner@gmail.com>
2016-07-08 04:06:32 -0300

    added teoria models

--- teoria/models.py
+++ teoria/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from grupos.models import Grupo
+
+
+class Unidad(models.Model):
+    numero = models.IntegerField()
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, default=None)
+
+    def __str__(self):
+        return 'U{} - {}'.format(self.numero, self.titulo)
+
+
+class Pregunta(models.Model):
+    unidad = models.ForeignKey(Unidad)
+    pregunta = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    vigente = models.BooleanField(default=True)
+
+
+class Material(models.Model):
+    unidad = models.ForeignKey(Unidad)
+    nombre = models.CharField(max_length=200)
+    link = models.URLField()
+    autor = models.CharField(max_length=200)
+    vigente = models.BooleanField(default=True)
+
+
+class Voto(models.Model):
+    votante = models.ForeignKey(Grupo)
+    pregunta = models.ForeignKey(Pregunta, null=True)
+    material = models.ForeignKey(Material, null=True)
