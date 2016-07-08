184b32df44e9e03db4a6bb48f66726f380d3b3c3
Sofia Denner <sofi.denner@gmail.com>
2016-07-08 00:01:18 -0300

    added description and id folder fields in grupos model

--- grupos/models.py
+++ grupos/models.py
@@ -5,6 +5,8 @@ class Grupo(models.Model):
     numero = models.IntegerField()
     empresa = models.CharField(max_length=200)
     producto = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, default=None)
+    id_carpeta_drive = models.CharField(max_length=200, null=True, default=None)
 
     def __str__(self):
         return '{} - G{} - {} - {}'.format(str(self.año), self.numero, self.empresa, self.producto)
