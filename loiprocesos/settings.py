452cb5cb8c771f7dfcbb2fd3c9d1b9931a6643c4
Sofia Denner <sofi.denner@gmail.com>
2016-07-08 03:51:53 -0300

    added teoria app

--- loiprocesos/settings.py
+++ loiprocesos/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'clases',
     'grupos',
     'base',
+    'teoria',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ teoria/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TeoriaConfig(AppConfig):
+    name = 'teoria'
