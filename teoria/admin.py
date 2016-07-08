e84330d46bb53df0714a8419fca6c2bb6cd5cde6
Sofia Denner <sofi.denner@gmail.com>
2016-07-08 18:58:59 -0300

    added teoria models controler from admin

--- teoria/admin.py
+++ teoria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from teoria.models import Unidad, Pregunta, Material, Voto
+
+admin.site.register(Unidad)
+admin.site.register(Pregunta)
+admin.site.register(Material)
+admin.site.register(Voto)
 
 # Register your models here.
