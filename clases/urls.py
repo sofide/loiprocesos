75e1621f37993405c8fe06b88f6b90360edb4c2d
Sofia Denner <sofi.denner@gmail.com>
2016-07-10 19:37:27 -0300

    deleted useless url clases_add

--- clases/urls.py
+++ clases/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from clases.views import clases_home, clases_add, ver_clase, ver_exposicion
+from clases.views import clases_home,  ver_clase, ver_exposicion
 
 urlpatterns = [
     url(r'^$', clases_home, name='clases_home'),
-    url(r'^cargar$', clases_add, name='clases_add'),
     url(r'^(?P<pk>[0-9]+)/$', ver_clase, name='ver_clase'),
     url(r'^exposicion/(?P<expo_pk>[0-9]+)/$', ver_exposicion, name='ver_exposicion'),
 
