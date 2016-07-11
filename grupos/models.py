a9f072a326a50af52b786fbc25d5da4ef2510a76
Sofia Denner <sofi.denner@gmail.com>
2016-07-10 21:55:12 -0300

    add integrantes field to grupo

--- grupos/models.py
+++ grupos/models.py
@@ -7,6 +7,12 @@ class Grupo(models.Model):
     producto = models.CharField(max_length=200)
     descripcion = models.TextField(null=True, blank=True, default=None)
     id_carpeta_drive = models.CharField(max_length=200, null=True, blank=True,  default=None)
+    integrantes = models.ManyToManyField(
+        'auth.User',
+        through='Pertenencia',
+        through_fields=('grupo', 'usuario'),
+        related_name='grupos'
+    )
 
     def __str__(self):
         return '{} - G{} - {} - {}'.format(str(self.año), self.numero, self.empresa, self.producto)
@@ -15,3 +21,6 @@ class Grupo(models.Model):
 class Pertenencia(models.Model):
     grupo = models.ForeignKey(Grupo)
     usuario = models.ForeignKey('auth.User')
+
+    def __str__(self):
+        return '{} en {}'.format(self.usuario, self.grupo)
