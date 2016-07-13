ce7344816c957a39c0ed8c30daf771cb5f763e2a
Sofia Denner <sofi.denner@gmail.com>
2016-07-12 22:30:54 -0300

    fixed ordering in exposicion model

--- clases/models.py
+++ clases/models.py
@@ -37,7 +37,7 @@ class Exposicion(models.Model):
 
     class Meta:
         verbose_name_plural = "Exposiciones"
-        ordering = ['-clase', 'grupo', 'tp']
+        ordering = ['clase', 'grupo', 'tp']
 
     def __str__(self):
         return '{} - G{} - TP {}'.format(str(self.clase), self.grupo.numero, self.tp)
