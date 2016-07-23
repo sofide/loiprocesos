49f61a7e1fcca16d793d7cbca3323f1c81663125
Sofia Denner <sofi.denner@gmail.com>
2016-07-23 01:14:44 -0300

    add fecha and change verbose_name, in pregunta and material models, teoria app

--- teoria/models.py
+++ teoria/models.py
@@ -16,6 +16,7 @@ class Pregunta(models.Model):
     unidad = models.ForeignKey(Unidad)
     pregunta = models.CharField(max_length=200)
     autor = models.CharField(max_length=200, verbose_name="Sugerido por")
+    fecha = models.DateField()
     vigente = models.BooleanField(default=True)
 
 
@@ -24,6 +25,7 @@ class Material(models.Model):
     nombre = models.CharField(max_length=200)
     link = models.URLField()
     autor = models.CharField(max_length=200, null=True, verbose_name="Sugerido por")
+    fecha = models.DateField()
     vigente = models.BooleanField(default=True)
 
 
