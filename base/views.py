aa16a7631b27accd89bc034ef975a1b3f900ebde
Sofia Denner <sofi.denner@gmail.com>
2016-07-23 03:09:11 -0300

    added some info in home page

--- base/views.py
+++ base/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
+from clases.models import Clase, Exposicion
+from clases.graphics import tiempo_expo_graphic
+
+from teoria.models import Unidad
+
+
+
 def home(request):
-    return render(request, 'base/home.html', {})
+    clase = Clase.objects.first()
+    tiempos = None
+    exposiciones = Exposicion.objects.filter(clase=clase)\
+                                     .order_by('grupo__numero')\
+                                     .select_related('grupo')
+    expo_chart = [expo for expo in exposiciones
+                       if expo.start_expo and expo.start_ques and expo.finish_expo]
+
+    if expo_chart:
+        tiempos = tiempo_expo_graphic(expo_chart)
+
+
+    unidades = Unidad.objects.all()
+
+    return render(request, 'base/home.html', {'clase': clase,
+                                              'tiempos': tiempos,
+                                              'unidades': unidades,
+                                             })
 
 
 def pruebas(request):
