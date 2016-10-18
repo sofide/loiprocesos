a6268a2f4308267827a212b4547e90b14ec343e4
Sofia Denner <sofi.denner@gmail.com>
2016-10-18 00:06:51 -0300

    added data to evaluacion admin

--- grupos/admin.py
+++ grupos/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from grupos.models import Grupo, Pertenencia, Autoevaluacion_grupal, Evaluacion, Criterio_evaluacion
 
+
+class AdminEvaluacion(admin.ModelAdmin):
+    list_display = ('evaluador', 'grupo_evaluado', 'criterio', 'puntuacion')
+
+
 admin.site.register(Grupo)
 admin.site.register(Pertenencia)
 admin.site.register(Autoevaluacion_grupal)
-admin.site.register(Evaluacion)
+admin.site.register(Evaluacion, AdminEvaluacion)
 admin.site.register(Criterio_evaluacion)
