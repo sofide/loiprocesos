3134ebd901ba66124ca51511c51ddb7eb2f7b96c
Sofia Denner <sofi.denner@gmail.com>
2018-09-24 20:02:57 -0300

    change local database settings

--- loiprocesos/settings.py
+++ loiprocesos/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'loiprocesos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database_local',
+        'USER': 'django_access',
+        'PASSWORD': 'password_to_access_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
