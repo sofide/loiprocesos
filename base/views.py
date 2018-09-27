24d194c70526e4c576a3e2d130e7ac7c96601072
Sofia Denner <sofi.denner@gmail.com>
2018-09-27 00:39:59 -0300

    new home page

--- base/views.py
+++ base/views.py
@@ -48,8 +48,50 @@ def home_original(request):
                                              })
 
 
-def home(request):
+def home_dos(request):
     return render(request, 'base/home.html')
+
+
+def home_tres(request):
+    def clase_data(exclude=None):
+        '''
+        Return data information about the first clase (except given id parameter)
+        '''
+        clase = Clase.objects.exclude(pk=exclude).first()
+        tiempos = None
+        preguntas = None
+        exposiciones = Exposicion.objects.filter(clase=clase)\
+                                         .order_by('grupo__numero')\
+                                         .select_related('grupo')
+        expo_chart = [expo for expo in exposiciones
+                           if expo.start_expo and expo.start_ques and expo.finish_expo]
+
+        preg_chart = [expo for expo in exposiciones
+                           if ContadorPreguntas.objects.filter(exposicion=expo).exists()]
+
+        if expo_chart:
+            tiempos = tiempo_expo_graphic(expo_chart)
+
+        if preg_chart:
+            preguntas = q_pregs_expos_graphic(preg_chart)
+
+        return clase, tiempos, preguntas
+
+    # Datos de la última clase
+    clase, tiempos, preguntas = clase_data()
+    # Datos de la anteúltima clase
+    clase_ant, tiempos_ant, preguntas_ant = clase_data(clase.pk)
+
+    unidades = Unidad.objects.all()
+
+    return render(request, 'base/home_tres.html', {'clase': clase,
+                                                   'tiempos': tiempos,
+                                                   'preguntas': preguntas,
+                                                   'clase_ant': clase_ant,
+                                                   'tiempos_ant': tiempos_ant,
+                                                   'preguntas_ant': preguntas_ant,
+                                                   'unidades': unidades,
+                                                  })
 
 
 def pruebas(request):
--- loiprocesos/urls.py
+++ loiprocesos/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from base.views import home, pruebas, home_original
+from base.views import home_dos, pruebas, home_original, home_tres
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^$', home, name='home'),
+    url(r'^$', home_tres, name='home'),
     url(r'^pruebas/$', pruebas, name='pruebas'),
     url(r'^clases/', include('clases.urls')),
     url(r'^grupos/', include('grupos.urls')),
     url(r'^teoria/', include('teoria.urls')),
     url(r'^gallery/', include('gallery.urls')),
     url(r'^1/', home_original, name='home_original'),
-    url(r'^2/', home, name='home2'),
+    url(r'^2/', home_dos, name='home2'),
+    url(r'^3/', home_tres, name='home3'),
 ]
